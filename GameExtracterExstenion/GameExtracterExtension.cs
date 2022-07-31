4197832248cb22ce6f15851182f2884a3e4eb114
Cruz Sanchez <cruzsanchez9209@gmail.com>
2022-07-31 01:07:23 -0500

    Add project files.

---
+++ GameExtracterExstenion/GameExtracterExtension.cs
@@ -0,0 +1,40 @@
+﻿using SharpShell.SharpContextMenu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameExtracterExstenion
+{
+    
+    internal class GameExtracterExtension : SharpContextMenu
+    {
+        protected override bool CanShowMenu()
+        {
+            return true;
+        }
+
+        protected override ContextMenuStrip CreateMenu()
+        {
+            var menu = new ContextMenuStrip();
+
+            var extract = new ToolStripMenuItem()
+            {
+                Text = "Extract Games"
+            };
+
+            extract.Click += (s, e) => ExtractGames();
+
+            menu.Items.Add(extract);
+
+            return menu;
+        }
+
+        private void ExtractGames()
+        {
+            
+        }
+    }
+}
