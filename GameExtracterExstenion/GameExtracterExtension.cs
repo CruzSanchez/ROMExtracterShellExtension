d110e061a33e0c599c20d498f4652bb196ba2587
Cruz Sanchez <cruzsanchez9209@gmail.com>
2022-07-31 01:27:11 -0500

    Mock extension finished, need to implement extract functionality

--- GameExtracterExstenion/GameExtracterExtension.cs
+++ GameExtracterExstenion/GameExtracterExtension.cs
@@ -1,14 +1,18 @@
-﻿using SharpShell.SharpContextMenu;
+﻿using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GameExtracterExstenion
 {
-    
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.ClassOfExtension, ".txt")]
     internal class GameExtracterExtension : SharpContextMenu
     {
         protected override bool CanShowMenu()
@@ -34,7 +38,19 @@ namespace GameExtracterExstenion
 
         private void ExtractGames()
         {
-            
+            //  Builder for the output
+            var builder = new StringBuilder();
+
+            //  Go through each file
+            foreach (var filePath in SelectedItemPaths)
+            {
+                //  Count the lines
+                builder.AppendLine(string.Format("{0} - {1} Lines",
+                  Path.GetFileName(filePath), File.ReadAllLines(filePath).Length));
+            }
+
+            //  Show the output
+            MessageBox.Show(builder.ToString());
         }
     }
 }
