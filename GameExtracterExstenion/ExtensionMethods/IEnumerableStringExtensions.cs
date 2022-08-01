a68d382ea9f871a69af20459f0b230a41703c3ef
Cruz Sanchez <cruzsanchez9209@gmail.com>
2022-08-01 02:43:46 -0500

    Added 2 extension methods for checking if the ROM file extensions exist

---
+++ GameExtracterExstenion/ExtensionMethods/IEnumerableStringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameExtracterExstenion.ExtensionMethods
+{
+    internal static class IEnumerableStringExtensions
+    {
+        /// <summary>
+        /// This will check to see if any of the selected folders contain files that have ROM file extensions
+        /// </summary>
+        /// <param name="files">An Enumerable collection of files to check</param>
+        /// <param name="extensions">An array of extensions to check for</param>
+        /// <returns>This returns true if there is at least one file that has a ROM extension</returns>
+        public static bool ContainsAny(this IEnumerable<string> files, string[] extensions)
+        {
+            foreach (var file in files)
+            {
+                foreach (var ext in extensions)
+                {
+                    if (file.Contains(ext))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ GameExtracterExstenion/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameExtracterExstenion.ExtensionMethods
+{
+    internal static class StringExtensions
+    {
+        /// <summary>
+        /// This method will check to see if "this" string ends with any of the specified file extensions from a provided array.
+        /// </summary>
+        /// <param name="s">The string to check</param>
+        /// <param name="extensions">An array, containing file extensions to check for.</param>
+        /// <returns>Returns true if "this" string ends with one of the file extensions.</returns>
+        public static bool EndsWithAny(this string s, string[] extensions) 
+        {
+            foreach (var ext in extensions)
+            {
+                if (s.EndsWith(ext))
+                {
+                    return true;
+                }
+            }
+
+            return false;             
+        }
+    }
+}
