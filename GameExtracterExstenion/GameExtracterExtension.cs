16d27c62983c552e1d8bba9b972be1ad2d350f07
Cruz Sanchez <cruzsanchez9209@gmail.com>
2022-08-01 02:44:45 -0500

    Added documentation to the main class and removed unneeded assets

--- GameExtracterExstenion/GameExtracterExtension.cs
+++ GameExtracterExstenion/GameExtracterExtension.cs
@@ -1,13 +1,10 @@
-﻿using SharpShell.Attributes;
+﻿using GameExtracterExstenion.ExtensionMethods;
+using SharpShell.Attributes;
 using SharpShell.SharpContextMenu;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GameExtracterExstenion
@@ -16,24 +13,31 @@ namespace GameExtracterExstenion
     [COMServerAssociation(AssociationType.Directory)]
     internal class GameExtracterExtension : SharpContextMenu
     {
+        //Array of ROM file extensions
+        private readonly string[] _extensions = new string[] { ".iso", ".gb", ".gba", ".nes", ".sfc", ".z64", ".bin", ".cue", ".n64", ".gcz", ".gbc" };
+
         protected override bool CanShowMenu()
         {
+            //Always show menu for directory types
             return true;
         }
 
         protected override ContextMenuStrip CreateMenu()
         {
+            //Creating a menu
             var menu = new ContextMenuStrip();
 
+            //Creating the command
             var extract = new ToolStripMenuItem()
             {
                 Text = "Extract Games",
-                ShortcutKeys = Keys.Control | Keys.B,
-                Image = Image.FromFile($@"{Directory.GetCurrentDirectory()}\extracter-removebg.png")                
+                ShortcutKeys = Keys.Control | Keys.B                               
             };
 
+            //Adding the ExtractGames() to the click event
             extract.Click += (s, e) => ExtractGames();
 
+            //Adding the command to the menu
             menu.Items.Add(extract);
 
             return menu;
@@ -41,28 +45,54 @@ namespace GameExtracterExstenion
 
         private void ExtractGames()
         {
-            foreach (var path in SelectedItemPaths.ToList())
+            //Get the paths of the selected folders
+            var paths = SelectedItemPaths;
+
+            foreach (var path in paths)
             {
+                //Get the parent directory for the selected folder(s)
                 var parent = Directory.GetParent(path).FullName;
 
-                foreach (var file in Directory.EnumerateFiles(path))
+                //Get all the file names in the selected folder(s)
+                var files = Directory.EnumerateFiles(path);
+
+                //If there are not any files with any of the ROM extensions, in the current folder, go to the next folder
+                if (!files.ContainsAny(_extensions))
                 {
-                    if (file.EndsWith(".sfc"))
+                    MessageBox.Show($"There are no game files in {path}");
+                    continue;
+                }
+
+                //Move the game files to the another folder (in this case one folder above the selected folder)
+                MoveFiles(files, parent);
+            }
+        }
+
+        private void MoveFiles(IEnumerable<string> files, string parentDirectory)
+        {
+            foreach (var file in files)
+            {
+                //Get the file info for the current file, this will be used to get the file path for the Move method
+                var fileInfo = new FileInfo(file);
+
+                //If the current file is a game file, move it to the parent folder
+                if (file.EndsWithAny(_extensions))
+                {
+                    try
                     {
-                        var fileInfo = new FileInfo(file);
-                        try
-                        {
-                            File.Move(fileInfo.FullName, $@"{parent}\{fileInfo.Name}");
-                            MessageBox.Show($"File: {fileInfo.Name}, was moved to: {parent}");
-                        }
-                        catch (Exception e)
-                        {
-                            MessageBox.Show(e.Message);
-                            MessageBox.Show($@"((BROKEN))File: {fileInfo.Name}, was not moved to: {parent}");
-                        }
+                        File.Move(fileInfo.FullName, $@"{parentDirectory}\{fileInfo.Name}");
+                    }
+                    catch (Exception e)
+                    {
+                        /*
+                         * I am just catching any exception here and letting the user know what happened.
+                         * With this shell command, from what I have seen, throwing an exception really doesn't break anything
+                         * it is like nothing ever happened.
+                         */
+                        MessageBox.Show(e.Message);
                     }
                 }
-            }            
+            }
         }
     }
 }
